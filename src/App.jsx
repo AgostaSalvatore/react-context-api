90bef3c8a3e0ec09fc40e4b611fcfe18a737b744
AgostaSalvatore <agosta.boolean@gmail.com>
2025-05-19 15:33:27 +0200

    feat: implement posts context and basic routing structure with React Router

--- src/App.jsx
+++ src/App.jsx
@@ -11,23 +11,20 @@ import About from "./pages/About"
 import Posts from "./pages/posts/Posts"
 
 //context
-import CountContext from "./contexts/CountContext"
 
 function App() {
   return (
     <>
-      <CountContext.Provider value={{ count: 1 }}>
-        <BrowserRouter>
-          <Routes>
-            <Route Component={DefaultLayout}>
-              <Route path="/" Component={Homepage} />
-              <Route path="/posts" Component={Posts} />
-              <Route path="/contacts" Component={Contacts} />
-              <Route path="/about" Component={About} />
-            </Route>
-          </Routes>
-        </BrowserRouter>
-      </CountContext.Provider>
+      <BrowserRouter>
+        <Routes>
+          <Route Component={DefaultLayout}>
+            <Route path="/" Component={Homepage} />
+            <Route path="/posts" Component={Posts} />
+            <Route path="/contacts" Component={Contacts} />
+            <Route path="/about" Component={About} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
--- src/pages/posts/Posts.jsx
+++ src/pages/posts/Posts.jsx
@@ -4,17 +4,12 @@ import "bootstrap/dist/css/bootstrap.min.css"
 import posts from '../../data/posts'
 import PostsPage from '../../components/PostsPage'
 
-//context
-import CountContext from '../../contexts/CountContext'
-
 const Posts = () => {
-    let { count } = useContext(CountContext);
     return (
         <>
             <div className="container">
                 <div className="row">
                     <div className="col-12 mb-4">
-                        <p>Count from context: {count}</p>
                     </div>
                 </div>
                 {posts === null ? (
