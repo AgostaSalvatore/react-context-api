649282ea6bf378b7dba81e519c599fb7c48b728b
AgostaSalvatore <agosta.boolean@gmail.com>
2025-05-19 14:22:28 +0200

    [UPD] created Contacts.jsx

---
+++ src/pages/Contacs.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Contacts = () => {
+    return (
+        <>
+            <div>
+                <h1>
+                    Contatti
+                </h1>
+            </div>
+        </>
+    )
+}
+
+export default Contacts
