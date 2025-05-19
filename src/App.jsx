ff410d4e2bff6747eb0019be8ad9c127202dce54
AgostaSalvatore <agosta.boolean@gmail.com>
2025-05-19 14:15:02 +0200

    [UPD] created base route path and created default layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import React from "react"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 
 function App() {
-
   return (
     <>
-
+      <BrowserRouter>
+        <Routes>
+          <Route Component={DefaulLayout}>
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
---
+++ src/layouts/DefaultLayout.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Outlet } from 'react-router-dom'
+
+const DefaultLayout = () => {
+    return (
+        <>
+            <header>
+            </header>
+            <main>
+                <div className="container">
+                    <div className="row">
+                        <div className="col-12 col-md-8">
+                            <Outlet />
+                        </div>
+                        <div className="col-12 col-md-4">
+                            Barra di navigazione laterale
+                        </div>
+                    </div>
+                </div>
+            </main>
+        </>
+    )
+}
