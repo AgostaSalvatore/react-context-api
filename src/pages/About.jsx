485ee1724e86cca3e68451cb4becf0c460d942df
AgostaSalvatore <agosta.boolean@gmail.com>
2025-05-19 14:23:17 +0200

    [UPD] created About

---
+++ src/pages/About.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import "bootstrap/dist/css/bootstrap.min.css"
+
+const About = () => {
+    return (
+        <>
+            <div className="row">
+                <div className="col-12 col-md-8">
+                    <h1>About</h1>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default About
